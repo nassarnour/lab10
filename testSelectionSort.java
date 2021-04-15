5765f778fbb47e14aec7ffa465f5469bf5e27afd
nour <nassarnour98@gmail.com>
2021-04-15 16:11:17 -0400

    add test page

---
+++ testSelectionSort.java
@@ -0,0 +1,17 @@
+package selectionsort;
+
+import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class testSelectionSort {
+	@Test
+	public void test() {
+		testPositive();
+		testNegative();
+		testMixed();
+		testDuplicates();
+	}
+
+    public testSelectionSort() {
+    }
